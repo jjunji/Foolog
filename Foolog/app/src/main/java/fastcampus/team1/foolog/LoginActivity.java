0b8adfad504e1bf45b99322794a05a3da8085afd
jjunji <jh.jun92@gmail.com>
2017-08-01 19:25:30 +0900

    auto login

--- Foolog/app/src/main/java/fastcampus/team1/foolog/LoginActivity.java
+++ Foolog/app/src/main/java/fastcampus/team1/foolog/LoginActivity.java
@@ -1,6 +1,8 @@
 package fastcampus.team1.foolog;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,6 +35,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button btnLogin, btnSignUp;
     Intent intent;
     String token;
+    String loginId, loginPwd;  // SharedPreferences 사용을 위한 id, pwd 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +52,42 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnLogin.setOnClickListener(this);
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
         btnSignUp.setOnClickListener(this);
+
+        SharedPreferences storage = getSharedPreferences("storage", Activity.MODE_PRIVATE);
+        // 처음에는 SharedPreferences에 아무런 정보도 없으므로 값을 저장할 키를 생성한다.
+        // getString 의 첫번째 인자는 저장될 키, 두번째 인자는 값.
+        // 처음에는 값이 없으므로 키 값은 원하는 것으로 하고, 값은 null 을 준다.
+        loginId = storage.getString("inputId",null);
+        loginPwd = storage.getString("inputPwd",null);
+
+        checkStorage();
+    }
+
+    private void checkStorage() {
+        if(loginId != null && loginPwd != null) {
+            Toast.makeText(LoginActivity.this, loginId +"님 자동로그인 입니다.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        /*else if (loginId == null && loginPwd == null){
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //if (id.getText().toString().equals("부르곰") && pwd.getText().toString().equals("네이버")) {
+
+                        //아이디가 '부르곰'이고 비밀번호가 '네이버'일 경우 SharedPreferences.Editor를 통해
+                        //auto의 loginId와 loginPwd에 값을 저장.
+
+
+
+                        Toast.makeText(LoginActivity.this, txtEmail.getText().toString()+"님 환영합니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+            });
+        }*/
     }
 
 
@@ -87,6 +126,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             Log.e("LoginActivity", "token =====================" +token);
                             Log.e("LoginActivity", "pk=========================" + pk);
                             Toast.makeText(getBaseContext(),"Success", Toast.LENGTH_SHORT).show();
+
+                            // ----------------------
+
+                            SharedPreferences storage = getSharedPreferences("storage", Activity.MODE_PRIVATE);
+                            SharedPreferences.Editor autoLogin = storage.edit();
+
+                            autoLogin.putString("inputId", txtEmail.getText().toString());
+                            autoLogin.putString("inputPwd", txtPassword.getText().toString());
+
+                            // commit() : 저장
+                            autoLogin.commit();
+
                             intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
                         }else{
