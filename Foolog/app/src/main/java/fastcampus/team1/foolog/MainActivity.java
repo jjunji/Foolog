f4cd230ceecd27f164f4eb9575468117425a222a
SeungHo Shin <94inusz@gmail.com>
2017-08-02 16:57:48 +0900

    MainActivity 권한 요청 추가

--- Foolog/app/src/main/java/fastcampus/team1/foolog/MainActivity.java
+++ Foolog/app/src/main/java/fastcampus/team1/foolog/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -18,8 +19,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import fastcampus.team1.foolog.util.PermissionControl;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, PermissionControl.CallBack {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // 앱 실행시 권한을 사용하게끔 나타낸다.
+        PermissionControl.checkVersion(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -120,5 +126,24 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * 권한 요청을 위한 함수
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionControl.onResult(this, requestCode, grantResults);
+    }
+
+    /**
+     * 권한 설정을 위한 callback함수 (interface)
+     * 만약 권한을 설정을 해준다면 버튼이 동작하게 만들어준다.
+     * todo 처음에는 실행이 잘 되지만 껏다가 다시 키면 에러가 뜬다
+     */
+    @Override
+    public void init() {
+
     }
 }
